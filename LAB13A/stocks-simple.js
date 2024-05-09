d726f00b9c1f6eeba372f6e2e6e7d106d19ea237
june0922 <miinjun0922@inha.edu>
2024-05-09 09:39:59 +0900

    저장

--- LAB13A/stocks-simple.js
+++ LAB13A/stocks-simple.js
@@ -11,3 +11,12 @@ const jsonData = fs.readFileSync(jsonPath, 'utf8');
 const stocks = JSON.parse(jsonData);
 // create an express app
 const app = express();
+
+// define the API routes
+// return all the stocks when a root request arrives
+app.get('/', (req,resp) => { resp.json(stocks) } );
+// Use express to listen to port
+let port = 8080;
+app.listen(port, () => {
+console.log("Server running at port= " + port);
+});
